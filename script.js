9b03e170f0747d5567d3a80d525dbf6208ebafb5
Chelsea <88430660+ChelseaChanu@users.noreply.github.com>
2023-04-15 11:51:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your code here
+const message = document.getElementById("evaluatedText");
+const count = document.getElementById("letterCount");
+count.textContent = "0";
+
+message.addEventListener("input",()=>{
+	count.textContent = message.value.trim().length;
+});
